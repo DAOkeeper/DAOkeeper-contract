dd877c1dac542dd07a8bd875e78f7a6887d2ae11
Junha <vjunhav@gmail.com>
2022-10-08 18:35:25 -0500

    New : added ERC20Trackable governance token contract for DAOkeeper @DAOkeeperToken.sol

---
+++ contracts/DAOkeeperToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC20/ERC20Trackable.sol";
+
+
+// DAO governance token deployment & mint token to the token contract itself
+contract DAOForceToken is ERC20Trackable {
+    
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _DAOName,
+        string memory _intro,
+        string memory _image,
+        string memory _link,
+        uint256 _initial_supply
+    )  ERC20Trackable (
+            _name,
+            _symbol,
+            _DAOName,
+            _intro,
+            _image,
+            _link,
+            _initial_supply
+    ) {
+        _mint(address(this), _initial_supply * 10 ** uint(decimals()));
+    }
+}
