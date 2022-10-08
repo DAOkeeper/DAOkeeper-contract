58a5c78e838508b22b0c3aed8c1ae73e386fbc3d
Junha <vjunhav@gmail.com>
2022-10-08 15:59:08 -0500

    New : added BalanceCommit structure for tracking ERC20 transfers @contracts/common/CommonStruct.sol

---
+++ contracts/common/CommonStructs.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library CommonStructs {
+    // BalanceCommit snapshot record
+    // triggered by token._transfer() / executeAirdropRound() call.
+    struct BalanceCommit {
+        uint32 blockNumber;
+        uint256 balanceCommit;
+    }
+}
