fca75f8bdf61b4366ba0c2f8a1ce9dfe87079d90
Junha <vjunhav@gmail.com>
2022-10-09 00:26:44 -0500

    New : added rpc method call script for ContractInfoStore contract @rpc/interactContractInfoStore.js

---
+++ rpc/interactContractInfoStore.js
@@ -0,0 +1,32 @@
+const { abi } = require('../abi/contractInfoStoreABI');
+const { addresses, rpcProviderUrl } = require('./utils/deployInfo');
+const { varNameToString, sendTransaction } = require('./utils/transactionSender');
+
+const ethers = require('ethers');
+
+const provider = new ethers.providers.JsonRpcProvider(rpcProviderUrl);
+
+const address = addresses.ContractInfoStore;
+
+const privateKey = process.env.KLAYTN_CHARLIE_PRIVATE_KEY;  // Charlie
+
+const wallet = new ethers.Wallet(privateKey,provider);
+
+const contract = new ethers.Contract(address,abi,wallet);
+
+/**
+ * Contract methods promise
+*/
+const TOKEN_ADDRESS = addresses.DAOkeeperToken;
+const USER_ADDRESS = process.env.KLAYTN_CHARLIE_ADDRESS;
+
+const getAllGovernanceTokenInfoPromise = contract.getAllGovernanceTokenInfo();
+const findGovernanceTokenListIdByAddrPromise = contract.findGovernanceTokenListIdByAddr(TOKEN_ADDRESS);
+const findAirdropTokenAddressListByUserAddrPromise = contract.findAirdropTokenAddressListByUserAddr(USER_ADDRESS);
+
+/**
+ * Send method call transactions
+*/
+sendTransaction(getAllGovernanceTokenInfoPromise, varNameToString({ getAllGovernanceTokenInfoPromise }));
+sendTransaction(findGovernanceTokenListIdByAddrPromise, varNameToString({ findGovernanceTokenListIdByAddrPromise }));
+sendTransaction(findAirdropTokenAddressListByUserAddrPromise, varNameToString({ findAirdropTokenAddressListByUserAddrPromise }));
