a8cee946e27a168b5c887f08b06ee5e3888e7dab
Junha <vjunhav@gmail.com>
2022-10-08 21:33:14 -0500

    Update : contract deployment metadata stored @rpc/utils/deployInfo.js

---
+++ rpc/utils/deployInfo.js
@@ -0,0 +1,10 @@
+// [key: contractName], [value: contractAddress]
+// network: Klaytn Baobab testnet
+
+const addressObject = {
+    DAOkeeperToken: "0xa4632c39c508f050a7ac6abb0bbc7a30585706d0",
+    VestingAirdrop: "",
+};
+
+exports.addresses = addressObject;
+exports.rpcProviderUrl = "https://public-node-api.klaytnapi.com/v1/baobab";  // Klaytn Baobab testnet
